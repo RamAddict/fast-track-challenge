f701b8b95b7ab7cc3cb72467418fd5677d25b3bb
ramaddict <apbiancofriber@gmail.com>
2026-01-29 15:41:51 -0300

    chore: remove default AppController and AppService boilerplate files and their references from AppModule.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,6 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ScheduleModule } from '@nestjs/schedule';
 import { LoggerModule } from 'nestjs-pino';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ShipmentsModule } from './shipments/shipments.module';
 import { DatabaseModule } from './db/database.module';
 import { loggerConfig } from './logger/logger.config';
@@ -20,7 +18,7 @@ import { QueueModule } from './queue/queue.module';
     DatabaseModule,
     ShipmentsModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
