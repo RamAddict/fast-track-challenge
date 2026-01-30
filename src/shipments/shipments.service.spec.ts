5cc6927727152ae69d37e01d312e6ab583a4efb5
ramaddict <apbiancofriber@gmail.com>
2026-01-30 18:05:49 -0300

    test: Add unit test for ShipmentsService.create

--- src/shipments/shipments.service.spec.ts
+++ src/shipments/shipments.service.spec.ts
@@ -3,9 +3,11 @@ import { ShipmentsService } from './shipments.service';
 import { PinoLogger } from 'nestjs-pino';
 import { ShipmentsRepository } from './repository/shipments.repository';
 import { CarrierService } from '../carrier/carrier.service';
+import { CreateShipmentDto } from './dto/create-shipment.dto';
 
 describe('ShipmentsService', () => {
   let service: ShipmentsService;
+  let repository: ShipmentsRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -41,9 +43,25 @@ describe('ShipmentsService', () => {
     }).compile();
 
     service = module.get<ShipmentsService>(ShipmentsService);
+    repository = module.get<ShipmentsRepository>(ShipmentsRepository);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should create a shipment equal to the shipment passed ', async () => {
+    const shipment = {
+      orderId: '1',
+      customerName: 'John Doe',
+      destination: 'mars',
+      status: 'pending',
+    };
+
+    jest.spyOn(repository, 'create').mockResolvedValue([shipment] as any);
+
+    await expect(
+      service.create(shipment as CreateShipmentDto),
+    ).resolves.toEqual(shipment);
   });
 });
